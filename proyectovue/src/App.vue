a8a6d12045369ff0a6bc553ded6e7d372cac44f5
bhernandezperez6565-ui <bhernandezperez65@gmail.com>
2025-09-16 10:41:52 -0600

    Menu

--- proyectovue/src/App.vue
+++ proyectovue/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Menú de navegación -->
+    <Navbar />
+
+    <!-- Aquí se mostrarán las páginas según la ruta -->
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
@@ -21,6 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
